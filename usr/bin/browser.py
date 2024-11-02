4c24883c4371b04cb25aaa6ddd4e0ec5f2456788
APOLLO Community Main Dev (casanovaltd) <123438051+casanovalx@users.noreply.github.com>
2024-11-02 13:02:07 +0300

    Add files via upload

---
+++ usr/bin/browser.py
@@ -0,0 +1,56 @@
+import os
+import sys
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtWebEngineWidgets import *
+from PyQt6.QtGui import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("https://www.duckduckgo.com/"))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+        # navigation bar
+
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+
+        back_btn = QAction("Back", self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
+
+        forward_btn = QAction("Forward", self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
+
+        refresh_btn = QAction("Refresh", self)
+        refresh_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(refresh_btn)
+
+        home_btn = QAction("Home", self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        self.url_bar = QLineEdit()
+        navbar.addWidget(self.url_bar)
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+
+        self.browser.urlChanged.connect(self.update_url)
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl("https://www.duckduckgo.com"))
+
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName("Gamma Browser")
+window = MainWindow()
+app.exec()
