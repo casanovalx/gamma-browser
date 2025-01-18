cb3df41ea81945babfb5d5f5e4df848c2a32d6a2
APOLLO Community Main Dev (casanovaltd) <123438051+casanovalx@users.noreply.github.com>
2025-01-18 21:55:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-echo 'Moving browser executable to /etc...'\ && sudo cp -r etc / && echo 'Moving browser files to /usr...'\ && sudo cp -r usr /
+echo 'Moving browser executable to /etc...'\ && sudo cp -r etc / && echo 'Moving browser files to /usr...'\ && sudo cp -r usr / && cd usr/bin && echo "Moving config files..."\ && mkdir /home/$USER/.config/gamma-browser && mv config.json /home/$USER/.config/gamma-browser && mv bookmarks.json /home/$USER/.config/gamma-browser && mv history.json /home/$USER/.config/gamma-browser && sudo update-desktop-database /usr/share/applications && echo "Done. Please restart the system."
