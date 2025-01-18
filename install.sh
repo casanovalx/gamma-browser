ddef614f594a6889c235f2d86d80f6eec5f3c20e
APOLLO Community Main Dev (casanovaltd) <123438051+casanovalx@users.noreply.github.com>
2025-01-18 18:04:09 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-echo 'Moving browser executable to /etc...\' && sudo mv etc / && echo 'Moving browser files to /usr...\' && sudo mv usr /
+echo 'Moving browser executable to /etc...'\ && sudo cp etc / && echo 'Moving browser files to /usr...'\ && sudo cp usr /
